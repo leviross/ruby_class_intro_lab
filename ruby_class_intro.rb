7269cba2d74c58e0408c5d4850c8bacf674557ba
leviross <levross@gmail.com>
2015-01-07 10:15:20 -0800

    updated homework

--- ruby_class_intro.rb
+++ ruby_class_intro.rb
@@ -43,65 +43,70 @@ p me.greet
 
 class Starship
 
-	@@topSpeed = 100
-	attr_accessor :currentSpeed
+	# edit some camelCase to snake_case
+	attr_accessor :current_speed, :topSpeed
 
-	def initialize model, ownerName
+	def initialize model, ownerName, topSpeed
+		@topSpeed = topSpeed
 		@model = model
 		@ownerName = ownerName
 	end
 
-	def topSpeed 
-		@@topSpeed
+	def get_top_speed 
+		@topSpeed
 	end
 
-	def setTopSpeed newTopSpeed
+	def set_top_speed newTopSpeed
 		@newTopSpeed = newTopSpeed
-		@@topSpeed = @newTopSpeed
+		@topSpeed = @newTopSpeed
 	end
 
-	def accelerateTo fast
-		@fast = fast
-		if (@fast < @@topSpeed) 
-			@currentSpeed = @fast
-			@currentSpeed
+	def accelerate_to increase
+		@increase = increase
+		if (@increase < @topSpeed) 
+			@current_speed = @increase
+			@current_speed
 		else 
-			"You can not drive faster than #{@@topSpeed}"	
+			"You can not drive faster than #{@topSpeed}"	
 		end
 	end
 
 end
 
-ship = Starship.new "ship", "Levi"
-p ship
-p ship.topSpeed
-p ship.setTopSpeed(150)
+ship = Starship.new "ship", "Levi", 100
+# p ship
+# p ship.get_top_speed
+# p ship.set_top_speed(150)
 
-# p ship.topSpeed
+# # p ship.topSpeed
 
-p ship.accelerateTo(201)
+# p ship.accelerate_to(201)
 
 
 # 4) Dice
 
 class Dice
-	@@prevRolls = []
+	
+	attr_accessor :number_of_sides
 
-	def initialize numberOfSides
-		@numberOfSides = numberOfSides
+	def initialize number_of_sides
+		@number_of_sides = number_of_sides
+		@prev_rolls = []
 	end
 
-	def prevRolls 
-		@@prevRolls
+	def prev_rolls 
+		@prev_rolls
 	end
 
 	def roll 
-		currentRoll = rand(1..@numberOfSides)
-		@@prevRolls.push(currentRoll)
+		currentRoll = rand(1..@number_of_sides)
+		@prev_rolls << currentRoll
+		currentRoll
+		
 	end
 
-	def getRolls 
-		@@prevRolls
+	def get_rolls 
+		@prev_rolls
 	end
 
 
@@ -110,13 +115,16 @@ end
 
 casino = Dice.new 6
 #casino2 = Dice.new 5
-casino.roll
-casino.roll
-casino.roll
+# casino.roll
+# casino.roll
+# casino.roll
 
+casino.number_of_sides.times do
+	puts casino.roll 
+	sleep 0.5	
+end
 
-p casino.getRolls
-#p casino2.prevRolls
+p casino.get_rolls
 
 
 
